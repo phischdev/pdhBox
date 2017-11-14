f5a3675a6c146eb99496b51894e778d2638bb74f
phischdev <phisch@oulook.de>
2017-11-14 15:34:56 +0100

    Menüs ausdünnen

--- electron/menu.js
+++ electron/menu.js
@@ -15,63 +15,82 @@ function sendAction(action) {
 
 	win.webContents.send(action);
 }
-module.exports = function(config) {
-	const locale = require('../resources/languages/'+config.get('locale'));
-	const helpSubmenu = [
-		// {
-		// 	label: `&`+locale['menu.help[0]'],
-		// 	click() {
-		// 		shell.openExternal('http://rambox.pro');
-		// 	}
-		// },
-		// {
-		// 	label: `&Facebook`,
-		// 	click() {
-		// 		shell.openExternal('https://www.facebook.com/ramboxapp');
-		// 	}
-		// },
-		// {
-		// 	label: `&Twitter`,
-		// 	click() {
-		// 		shell.openExternal('https://www.twitter.com/ramboxapp');
-		// 	}
-		// },
-		// {
-		// 	label: `&GitHub`,
-		// 	click() {
-		// 		shell.openExternal('https://www.github.com/saenzramiro/rambox');
-		// 	}
-		// },
-		// {
-		// 	type: 'separator'
-		// },
+module.exports = function (config) {
+	const locale = require('../resources/languages/' + config.get('locale'));
+	// const helpSubmenu = [
 	// 	{
-	// 		label: '&'+locale['menu.help[1]'],
-	// 		click() {
-	// 			const body = `
-	// <!-- Please describe here your issue and steps to reproduce it. -->
-	// <!-- DON'T REMOVE THE FOLLOWING LINES -->
-	// -
-	// > ${app.getName()} ${app.getVersion()}
-	// > Electron ${process.versions.electron}
-	// > ${process.platform} ${process.arch} ${os.release()}`;
-    //
-	// 			shell.openExternal(`https://github.com/saenzramiro/rambox/issues/new?body=${encodeURIComponent(body)}`);
-	// 		}
+	// 		label: `&Tools`,
+	// 		submenu: [
+
+	// 		]
 	// 	},
+	// ];
+
+	let tpl = [
 		// {
-		// 	label: `&`+locale['menu.help[2]'],
-		// 	click() {
-		// 		shell.openExternal('https://gitter.im/saenzramiro/rambox');
-		// 	}
+		// 	label: '&'+locale['menu.edit[0]'],
+		// 	submenu: [
+		// 		{
+		// 			 role: 'undo'
+		// 			,label: locale['menu.edit[1]']
+		// 		},
+		// 		{
+		// 			 role: 'redo'
+		// 			,label: locale['menu.edit[2]']
+		// 		},
+		// 		{
+		// 			type: 'separator'
+		// 		},
+		// 		{
+		// 			 role: 'cut'
+		// 			,label: locale['menu.edit[3]']
+		// 		},
+		// 		{
+		// 			 role: 'copy'
+		// 			,label: locale['menu.edit[4]']
+		// 		},
+		// 		{
+		// 			 role: 'paste'
+		// 			,label: locale['menu.edit[5]']
+		// 		},
+		// 		{
+		// 			role: 'pasteandmatchstyle'
+		// 		},
+		// 		{
+		// 			 role: 'selectall'
+		// 			,label: locale['menu.edit[6]']
+		// 		},
+		// 		{
+		// 			role: 'delete'
+		// 		}
+		// 	]
 		// },
 		{
-			label: `&Tools`,
+			label: '&Probleme?',
 			submenu: [
+				{
+					label: `&` + locale['menu.help[6]'],
+					click() {
+						sendAction('showAbout')
+					}
+				},
+				{
+					type: 'separator'
+				},
+				{
+					label: '&' + locale['menu.view[1]'],
+					accelerator: 'CmdOrCtrl+R',
+					click(item, focusedWindow) {
+						if (focusedWindow) focusedWindow.reload();
+					}
+				},
+				{
+					type: 'separator'
+				},
 				{
 					label: `&Clear Cache`,
 					click(item, win) {
-						win.webContents.session.clearCache(function() {
+						win.webContents.session.clearCache(function () {
 							win.reload();
 						});
 					}
@@ -81,83 +100,33 @@ module.exports = function(config) {
 					click(item, win) {
 						win.webContents.session.clearStorageData({
 							storages: ['localstorage']
-						}, function() {
+						}, function () {
 							win.reload();
 						});
 					}
 				}
-			]
-		},
-		// {
-		// 	type: 'separator'
-		// },
-		// {
-		// 	label: `&`+locale['menu.help[3]'],
-		// 	click() {
-		// 		shell.openExternal('https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=WU75QWS7LH2CA');
-		// 	}
-		// }
-	];
 
-	let tpl = [
-		{
-			label: '&'+locale['menu.edit[0]'],
-			submenu: [
-				{
-					 role: 'undo'
-					,label: locale['menu.edit[1]']
-				},
-				{
-					 role: 'redo'
-					,label: locale['menu.edit[2]']
-				},
-				{
-					type: 'separator'
-				},
-				{
-					 role: 'cut'
-					,label: locale['menu.edit[3]']
-				},
-				{
-					 role: 'copy'
-					,label: locale['menu.edit[4]']
-				},
-				{
-					 role: 'paste'
-					,label: locale['menu.edit[5]']
-				},
-				{
-					role: 'pasteandmatchstyle'
-				},
-				{
-					 role: 'selectall'
-					,label: locale['menu.edit[6]']
-				},
-				{
-					role: 'delete'
-				}
 			]
 		},
+		// {
+		// 	label: '&' + locale['menu.view[0]'],
+		// 	submenu: [
+		// 		// {
+		// 		// 	label: '&Reload current Service',
+		// 		// 	accelerator: 'CmdOrCtrl+Shift+R',
+		// 		// 	click() {
+		// 		// 		sendAction('reloadCurrentService');
+		// 		// 	}
+		// 		// },
+		// 		// {
+		// 		// 	type: 'separator'
+		// 		// },
+		// 	]
+		// },
 		{
-			label: '&'+locale['menu.view[0]'],
+			label: '&' + locale['menu.window[0]'],
+			role: 'window',
 			submenu: [
-				{
-					label: '&'+locale['menu.view[1]'],
-					accelerator: 'CmdOrCtrl+R',
-					click(item, focusedWindow) {
-						if (focusedWindow) focusedWindow.reload();
-					}
-				},
-				{
-					label: '&Reload current Service',
-					accelerator: 'CmdOrCtrl+Shift+R',
-					click() {
-						sendAction('reloadCurrentService');
-					}
-				},
-				{
-					type: 'separator'
-				},
 				{
 					role: 'zoomin'
 				},
@@ -166,32 +135,33 @@ module.exports = function(config) {
 				},
 				{
 					role: 'resetzoom'
-				}
-			]
-		},
-		{
-			label: '&'+locale['menu.window[0]'],
-			role: 'window',
-			submenu: [
-				{
-					label: '&'+locale['menu.window[1]'],
-					accelerator: 'CmdOrCtrl+M',
-					role: 'minimize'
-				},
-				{
-					label: '&'+locale['menu.window[2]'],
-					accelerator: 'CmdOrCtrl+W',
-					role: 'close'
 				},
 				{
 					type: 'separator'
 				},
 				{
-					 role: 'togglefullscreen'
-					,label: locale['menu.view[2]']
+					role: 'quit',
+					label: locale['menu.file[1]']
+				},
+				// {
+				// 	label: '&' + locale['menu.window[1]'],
+				// 	accelerator: 'CmdOrCtrl+M',
+				// 	role: 'minimize'
+				// },
+				// {
+				// 	label: '&' + locale['menu.window[2]'],
+				// 	accelerator: 'CmdOrCtrl+W',
+				// 	role: 'close'
+				// },
+				{
+					type: 'separator'
 				},
 				{
-					label: '&'+locale['menu.view[3]'],
+					role: 'togglefullscreen'
+					, label: locale['menu.view[2]']
+				},
+				{
+					label: '&' + locale['menu.view[3]'],
 					accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
 					click(item, focusedWindow) {
 						if (focusedWindow) focusedWindow.webContents.toggleDevTools();
@@ -199,13 +169,13 @@ module.exports = function(config) {
 				}
 			]
 		},
-		{
-			label: '&'+locale['menu.help[4]'],
-			role: 'help'
-		}
+		// {
+		// 	label: '&' + locale['menu.help[4]'],
+		// 	role: 'help'
+		// }
 	];
 
-	if (process.platform === 'darwin') {
+	if (process.platform !== 'darwin') {
 		tpl.unshift({
 			label: appName,
 			submenu: [
@@ -215,14 +185,14 @@ module.exports = function(config) {
 						sendAction('showPreferences')
 					}
 				},
-				{
-					label: locale['menu.help[5]'],
-					click(item, win) {
-						const webContents = win.webContents;
-						const send = webContents.send.bind(win.webContents);
-						send('autoUpdater:check-update');
-					}
-				},
+				// {
+				// 	label: locale['menu.help[5]'],
+				// 	click(item, win) {
+				// 		const webContents = win.webContents;
+				// 		const send = webContents.send.bind(win.webContents);
+				// 		send('autoUpdater:check-update');
+				// 	}
+				// },
 				{
 					label: locale['menu.help[6]'],
 					click() {
@@ -264,44 +234,38 @@ module.exports = function(config) {
 			]
 		});
 	} else {
-		tpl.unshift({
-			label: '&'+locale['menu.file[0]'],
-			submenu: [
-				{
-					label: locale['preferences[0]'],
-					click() {
-						sendAction('showPreferences')
-					}
-				},
-				{
-					type: 'separator'
-				},
-				{
-					role: 'quit',
-					label: locale['menu.file[1]']
-				}
-			]
-		});
-		helpSubmenu.push({
-			type: 'separator'
-		});
-		helpSubmenu.push({
-			label: `&`+locale['menu.help[5]'],
-			click(item, win) {
-				const webContents = win.webContents;
-				const send = webContents.send.bind(win.webContents);
-				send('autoUpdater:check-update');
-			}
-		});
-		helpSubmenu.push({
-			label: `&`+locale['menu.help[6]'],
-			click() {
-				sendAction('showAbout')
-			}
-		});
+		// tpl.unshift({
+		// 	label: '&' + locale['menu.file[0]'],
+		// 	submenu: [
+		// 		{
+		// 			label: locale['preferences[0]'],
+		// 			click() {
+		// 				sendAction('showPreferences')
+		// 			}
+		// 		},
+		// 		{
+		// 			type: 'separator'
+		// 		},
+		// 		{
+		// 			role: 'quit',
+		// 			label: locale['menu.file[1]']
+		// 		}
+		// 	]
+		// });
+		// helpSubmenu.push({
+		// 	type: 'separator'
+		// });
+		// helpSubmenu.push({
+		// 	label: `&`+locale['menu.help[5]'],
+		// 	click(item, win) {
+		// 		const webContents = win.webContents;
+		// 		const send = webContents.send.bind(win.webContents);
+		// 		send('autoUpdater:check-update');
+		// 	}
+		// });
 	}
 
-	tpl[tpl.length - 1].submenu = helpSubmenu;
+	// tpl[tpl.length - 1].submenu = helpSubmenu;
 
 	return electron.Menu.buildFromTemplate(tpl);
 };
